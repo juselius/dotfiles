cad6b26aa856c0d145380e1e4d4f1044747f6f25
Jonas Juselius <jonas.juselius@itpartner.no>
2019-11-19 16:40:34 +0100

    Disable gnupg file configuration

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -42,11 +42,11 @@ let
       target = ".ssh";
       recursive = true;
     };
-    gnupg = {
-      source = ~/.dotfiles/gnupg;
-      target = ".gnupg";
-      recursive = true;
-    };
+    # gnupg = {
+    #   source = ~/.dotfiles/gnupg;
+    #   target = ".gnupg";
+    #   recursive = true;
+    # };
   };
 
   extraDotfiles = [
