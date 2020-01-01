8820169ded0d23485b4d7b330c61cd6972ff4b69
Jonas Juselius <jonas.juselius@itpartner.no>
2020-01-01 15:50:27 +0100

    Update vscode to 1.41.1

--- config/nixpkgs/overlays/vscode.nix
+++ config/nixpkgs/overlays/vscode.nix
@@ -7,13 +7,13 @@ let
 in
 {
   vscode = super.vscode.overrideAttrs (attrs: rec {
-      version = "1.41.0";
+      version = "1.41.1";
       name = "vscode-${version}";
 
       src = super.fetchurl {
         name = "VSCode_${version}_${plat}.${archive_fmt}";
         url = "https://vscode-update.azurewebsites.net/${version}/${plat}/stable";
-        sha256 = "1ziw2b851kg17jaf713nwhsv5ikbvivq3h01xximhcglv9vzk93l";
+        sha256 = "00b6q3rvf9v993i8d1vh9ckb86s655mz6kqkw0lxg8xmhpm568ra";
       };
       # postFixup = ''
       #     wrapProgram $out/bin/code --prefix PATH : ${lib.makeBinPath [hie]}
