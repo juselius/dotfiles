227ac734847c615c6a9276fc053be3bc00b05e99
Jonas Juselius <jonas.juselius@itpartner.no>
2020-10-28 11:16:00 +0100

    Add custom fish prompt theme j

---
+++ config/fish/themes/j/fish_right_prompt.fish
@@ -0,0 +1,13 @@
+function fish_right_prompt -d "Write out the right prompt"
+  set -l _status $status
+  if [ $_status -gt 0 ]
+    echo -n -s (set_color red) "$_status " (set_color normal)
+  end
+  if [ ! -z SSH_CONNECTION ]
+    set -l h (set_color green)"$hostname"(set_color normal)
+    set -l at (set_color white)"@"(set_color normal)
+    set -l u (set_color yellow)"$USER"(set_color normal)
+    echo -n "$u$at$h"
+  end
+end
+
