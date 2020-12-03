f00ffdbcd39e01445f7a8213f401d5dfc521338e
Jonas Juselius <jonas.juselius@itpartner.no>
2020-12-03 11:41:37 +0100

    Change to stable home-manager release.

--- config/nixpkgs/home.nix
+++ config/nixpkgs/home.nix
@@ -223,7 +223,7 @@ in
 
     home-manager = {
       enable = true;
-      path = "https://github.com/rycee/home-manager/archive/master.tar.gz";
+      path = "https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz";
     };
   };
 
