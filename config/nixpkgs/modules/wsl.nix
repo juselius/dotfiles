79baa185ed1d53b8712d2117483857669ab1f457
Jonas Juselius <jonas.juselius@itpartner.no>
2021-01-09 10:41:56 +0100

    Move some wsl options to plainNix

--- config/nixpkgs/modules/wsl.nix
+++ config/nixpkgs/modules/wsl.nix
@@ -15,18 +15,7 @@ let
         '';
       };
     };
-
-    services = {
-      gpg-agent = {
-        enable = true;
-        enableSshSupport = true;
-        defaultCacheTtl = 43200; # 12 hours
-        defaultCacheTtlSsh = 64800; # 18 hours
-        maxCacheTtl = 64800;
-        maxCacheTtlSsh = 64800;
-        pinentryFlavor = "curses";
-      };
-    };
+    dotfiles.plainNix = true;
   };
 in {
   options.dotfiles.wsl = {
