e88c142e2a59c93775450bbf5baec06416fccfc7
Jonas Juselius <jonas.juselius@itpartner.no>
2021-02-05 09:07:33 +0100

    Update wavebox and vscode

--- config/nixpkgs/overlays/vscode.nix
+++ config/nixpkgs/overlays/vscode.nix
@@ -7,13 +7,13 @@ let
 in
 {
   vscode = super.vscode.overrideAttrs (attrs: rec {
-      version = "1.52.1";
+      version = "1.53.0";
       name = "vscode-${version}";
 
       src = super.fetchurl {
         name = "VSCode_${version}_${plat}.${archive_fmt}";
         url = "https://vscode-update.azurewebsites.net/${version}/${plat}/stable";
-        sha256 = "1kbjbqb03yapz7067q589gaa7d6cqaipj7hmp1l3nh0bmggzsc4c";
+        sha256 = "07wbh4sfvnmvif1a03z8bbqrjmcwmb4q5nn3l2r738i9wrxdxjja";
       };
       # postFixup = ''
       #     wrapProgram $out/bin/code --prefix PATH : ${lib.makeBinPath [hie]}
