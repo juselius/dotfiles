d1a41785de0fbedc8317de194148141cc0bfc000
Jonas Juselius <jonas.juselius@tromso.serit.no>
2021-03-07 09:06:49 +0100

    Fix gpg and gpg-agent for wsl

--- config/nixpkgs/modules/wsl.nix
+++ config/nixpkgs/modules/wsl.nix
@@ -4,6 +4,20 @@ let
   cfg = config.dotfiles.wsl;
 
   configuration = {
+    services = {
+      gpg-agent = {
+        enable = true;
+        enableSshSupport = true;
+        defaultCacheTtl = 43200; # 12 hours
+        defaultCacheTtlSsh = 64800; # 18 hours
+        maxCacheTtl = 64800;
+        maxCacheTtlSsh = 64800;
+        pinentryFlavor = "curses";
+        extraConfig = ''
+          pinentry-program ${pkgs.pinentry-curses}/bin/pinentry
+        '';
+      };
+    };
     programs.fish = {
       functions = {
         winpass = "pass $argv | head -1 | clip.exe";
@@ -15,7 +29,16 @@ let
         '';
       };
     };
+    programs.gpg = {
+      enable = true;
+      settings = {
+        use-agent = true;
+      };
+    };
     dotfiles.plainNix = true;
+    home.packages = [
+      pkgs.pass
+    ];
   };
 in {
   options.dotfiles.wsl = {
