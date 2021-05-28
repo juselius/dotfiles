23488da195cbcf54482601f44225efa7a0f31360
Jonas Juselius <jonas.juselius@itpartner.no>
2021-05-28 11:34:39 +0200

    Improve the j2 theme

--- config/fish/themes/j2/fish_prompt.fish
+++ config/fish/themes/j2/fish_prompt.fish
@@ -6,12 +6,12 @@ function _git_branch_name
 end
 
 function _is_git_dirty
-  command git status -s --ignore-submodules=dirty > /dev/null 2>&1
+  command git status -s --ignore-submodules=dirty 2> /dev/null | grep -q '^.'
   and echo "true"
 end
 
 function _is_git_staged
-  command git status -s --ignore-submodules=dirty 2> /dev/null | grep -q '^ A'
+  command git status -s --ignore-submodules=dirty 2> /dev/null | grep -q '^[AMDR]'
   and echo "true"
 end
 
@@ -36,6 +36,8 @@ function fish_prompt
   set -l blue (set_color blue)
   set -l green (set_color green)
   set -l yellow (set_color yellow)
+  set -l cyan (set_color cyan)
+  set -l magenta (set_color magenta)
   set -l normal (set_color normal)
 
   if [ -z "$SSH_TTY" ]
@@ -47,39 +49,53 @@ function fish_prompt
   set -g fish_prompt_pwd_dir_length 0
   set -l cwd $blue(prompt_pwd)
 
-  if [ (_git_branch_name) ]
+  set -l git_info
+  set -l git_branch (_git_branch_name)
+  if [ "$git_branch" ]
 
     if [ (_is_git_unmerged) ]
-        set git_info $red(_git_branch_name)
+        set git_info $red $git_branch
     else if [ (_is_git_stashed) ]
-        set git_info $blue(_git_branch_name)
+        set git_info $blue $git_branch
     else if [ (_is_git_untracked) ]
-        set git_info $yellow(_git_branch_name)
+        set git_info $yellow $git_branch
     else
-        set git_info $green(_git_branch_name)
+        set git_info $green $git_branch
     end
 
     if [ (_is_git_staged) ]
-      set git_info "$git_info" "+"
+      set git_info $git_info "+"
     else if [ (_is_git_dirty) ]
-      set git_info "$git_info" "*"
+      set git_info $git_info "*"
     end
 
+  end
+
+  set -l xu '╭'
+  set -l xl '╰'
+  # set -l xu '╭─'
+  # set -l xl '╰─'
+  # set -l xd '─'
+  set -l xd ' '
+  if [ "$IN_NIX_SHELL" ]
+     set -l xu $magenta $xu $normal
+     set -l xl $magenta $xl $normal
+     set -l xd $magenta $xd $normal
   end
 
   if [ ! -z "$SSH_TTY" ]
     set -l h (set_color green)"$hostname"(set_color normal)
     set -l at (set_color white)"@"(set_color normal)
     set -l u (set_color yellow)"$USER"(set_color normal)
-    echo "$u$at$h $cwd"
+    echo $xu "$u$at$h $cwd" $normal
   else
-    echo $cwd
+    echo $xu "$cwd" $normal
   end
 
-  if [ -z "$IN_NIX_SHELL" ]
-      echo -n -s $git_info $normal ' ' $arrow ' '
+  if [ -z "$git_info" ]
+      echo -n -s $xl ' ' $arrow ' '
   else
-      echo -n -s [ $git_info $normal ] ' ' $arrow ' '
+      echo -n -s $xl ' ' $git_info $normal $xd $arrow ' '
   end
 end
 
