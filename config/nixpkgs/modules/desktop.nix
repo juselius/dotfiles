7c0aedb0cc2110f9443d882c9c8cfe1c2163afcb
Jonas Juselius <jonas.juselius@itpartner.no>
2021-06-20 08:06:17 +0200

    Replace termite with alacritty

--- config/nixpkgs/modules/desktop.nix
+++ config/nixpkgs/modules/desktop.nix
@@ -108,58 +108,8 @@ let
       };
     };
 
-    programs.termite = {
+    programs.alacritty = {
       enable = true;
-      clickableUrl = false;
-      font = "Monospace 10";
-      foregroundColor         = "#d8d8d8";
-      foregroundBoldColor     = "#e8e8e8";
-      cursorColor             = "#e8e8e8";
-      cursorForegroundColor   = "#181818";
-      #backgroundColor        = "rgba(24, 24, 24, 1)";
-      colorsExtra = ''
-        # Base16 Default Dark
-        # Author: Chris Kempson (http://chriskempson.com)
-
-        # Black, Gray, Silver, White
-        color0  = #0b1c2c
-        #color0  = #181818
-        color8  = #585858
-        color7  = #d8d8d8
-        color15 = #f8f8f8
-
-        # Red
-        color1  = #ab4642
-        color9  = #ab4642
-
-        # Green
-        color2  = #a1b56c
-        color10 = #a1b56c
-
-        # Yellow
-        color3  = #f7ca88
-        color11 = #f7ca88
-
-        # Blue
-        color4  = #7cafc2
-        color12 = #7cafc2
-
-        # Purple
-        color5  = #ba8baf
-        color13 = #ba8baf
-
-        # Teal
-        color6  = #86c1b9
-        color14 = #86c1b9
-
-        # Extra colors
-        color16 = #dc9656
-        color17 = #a16946
-        color18 = #282828
-        color19 = #383838
-        color20 = #b8b8b8
-        color21 = #e8e8e8
-      '';
     };
   };
 
