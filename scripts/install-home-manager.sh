55a278fb7acf6c6516330d9842cc6d63ec8ed757
Jonas Juselius <jonas.juselius@tromso.serit.no>
2021-06-20 09:56:04 +0200

    Update install script to 21.05

--- scripts/install-home-manager.sh
+++ scripts/install-home-manager.sh
@@ -7,7 +7,7 @@ cd $top
 git submodule init
 git submodule update
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-20.09.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz home-manager
 nix-channel --update
 
 export NIX_PATH="home-manager=$HOME/.nix-defexpr/channels/home-manager${NIX_PATH:+:}$NIX_PATH"
