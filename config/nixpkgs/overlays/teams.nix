facd93d63e63ec6eb01c02978768604ec9755222
Jonas Juselius <jonas.juselius@itpartner.no>
2021-08-27 11:40:16 +0200

    Update teams overlay

--- config/nixpkgs/overlays/teams.nix
+++ config/nixpkgs/overlays/teams.nix
@@ -3,10 +3,10 @@ let
 in
 {
   teams = super.teams.overrideAttrs (attrs: rec {
-      version = "1.3.00.30857";
+      version = "1.4.00.13653";
       src = super.fetchurl {
       url = "https://packages.microsoft.com/repos/ms-teams/pool/main/t/teams/teams_${version}_amd64.deb";
-      sha256 = "06r48h1fr2si2g5ng8hsnbcmr70iapnafj21v5bzrzzrigzb2n2h";
+      sha256 = "1kx4j837fd344zy90nl0j3r8cdvihy6i6gf56wd5n56zngx1fhjv";
       };
   });
 }
