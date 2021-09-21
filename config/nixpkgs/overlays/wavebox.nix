ca4a5991a056c0ef3429baecd701268a72b0527e
Jonas Juselius <jonas.juselius@itpartner.no>
2021-09-21 07:50:02 +0200

    Update wavebox

--- config/nixpkgs/overlays/wavebox.nix
+++ config/nixpkgs/overlays/wavebox.nix
@@ -2,7 +2,7 @@ self: super:
 with super.lib;
 let
   # version = "10.0.554-2";
-  version = "10.93.11-2";
+  version = "10.93.12-2";
   tarball = "Wavebox_${version}.tar.gz";
   desktopItem = super.makeDesktopItem rec {
     name = "Wavebox";
@@ -18,7 +18,7 @@ in
     name = "wavebox-${version}";
     src = super.fetchurl {
       url = "https://download.wavebox.app/stable/linux/tar/${tarball}";
-      sha256 = "1yk6da384406lrw1s4jhj09xjr3rxprgzzynr4jnw57imhm3msgb";
+      sha256 = "1ffpv4pnydg3ivk753b5i4c3jsjvh6digfiyn5i93rjgwi28b16c";
     };
     buildInputs =
       attrs.buildInputs ++ [
