975ef5d1d1997a72cd119190baefefeb911ce224
Jonas Juselius <jonas.juselius@itpartner.no>
2021-12-09 09:17:32 +0100

    fix: update rider

--- config/nixpkgs/overlays/rider.nix
+++ config/nixpkgs/overlays/rider.nix
@@ -4,12 +4,12 @@ let
 in
 {
   rider = super.jetbrains.rider.overrideAttrs (attrs: rec {
-      version = "2021.2.2";
+      version = "2021.3";
       name = "rider-${version}";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-        sha256 = "17xx8mz3dr5iqlr0lsiy8a6cxz3wp5vg8z955cdv0hf8b5rncqfa";
+        sha256 = "0m1y770dsl8fyrvbmp1b56rka1jhanlxzdbp8pa0jx9fxjr9anmi";
       };
   });
 
