632591ba197fa3a04ef568e08a72e2b4426b1b5a
Jonas Juselius <jonas.juselius@itpartner.no>
2022-01-05 15:12:04 +0100

    breaking change: use i3 (soon sway) with tons of ricing

--- config/nixpkgs/modules/wm.nix
+++ config/nixpkgs/modules/wm.nix
@@ -60,7 +60,25 @@ let
     ];
   };
 
-  i3 = {
+  i3 =
+    let
+      base00 = "#101218";
+      base01 = "#1f222d";
+      base02 = "#252936";
+      base03 = "#7780a1";
+      base04 = "#C0C5CE";
+      base05 = "#d1d4e0";
+      base06 = "#C9CCDB";
+      base07 = "#ffffff";
+      base08 = "#ee829f";
+      base09 = "#f99170";
+      base0A = "#ffefcc";
+      base0B = "#a5ffe1";
+      base0C = "#97e0ff";
+      base0D = "#97bbf7";
+      base0E = "#c0b7f9";
+      base0F = "#fcc09e";
+    in {
     xsession.windowManager.i3 = {
       enable = true;
       config = {
@@ -70,40 +88,150 @@ let
         modifier = "Mod4";  # this is the "windows" key
         defaultWorkspace = "workspace number 1";
         assigns = {
-          "2:" = [{ class = "^Firefox$"; }];
+          "1" = [
+            { class = "^Mailspring$"; }
+            { class = "^Microsoft Teams - Preview"; }
+          ];
+          "2" = [
+            { class = "^Firefox$"; }
+            { class = "^google-chrome$"; }
+          ];
         };
+        floating.criteria = [ { title = "^zoom$"; } ];
         bars = [{
           position = "top";
+          statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-default.toml";
+          fonts = {
+            names = [ "DejaVu Sans Mono" "FontAwesome5Free" ];
+            style = "Normal";
+            size = 9.0;
+          };
+          colors = {
+            separator  = base03;
+            background = base01;
+            statusline = base05;
+            focusedWorkspace  = { background = base01; border = base01; text = base07; };
+            activeWorkspace   = { background = base01; border = base02; text = base03; };
+            inactiveWorkspace = { background = base01; border = base01; text = base03; };
+            urgentWorkspace   = { background = base01; border = base01; text = base08; };
+          };
         }];
-        floating.criteria = [ { title = "^zoom$"; } ];
+        modes.resize = {
+          Up = "resize shrink height 5 px or 5 ppt";
+          Down = "resize grow height 5 px or 5 ppt";
+          Left = "resize shrink width 5 px or 5 ppt";
+          Right = "resize grow width 5 px or 5 ppt";
+          Escape = "mode default";
+          Return = "mode default";
+        };
         startup = [
           { command = "${pkgs.i3-auto-layout}/bin/i3-auto-layout"; always = true; notification = false; }
         ];
         keybindings =
           let
             mod = config.xsession.windowManager.i3.config.modifier;
-            switch = n: "exec --no-startup-id ${pkgs.i3-wk-switch}/bin/i3-wk-switch ${toString n}";
-            wk_switch =
+            switch = n: "exec --no-startup-id ${pkgs.i3-wk-switch}/bin/i3-wk-switch ${n}";
+            switches =
               builtins.foldl' (a: x:
                 a // { "${mod}+${x}" = switch x; }
               ) {} (builtins.genList (x: toString x) 10);
           in lib.mkOptionDefault {
-            "${mod}+1" = switch 1;
-            "${mod}+2" = switch 2;
-            "${mod}+3" = switch 3;
-            "${mod}+4" = switch 4;
-            "${mod}+5" = switch 5;
-            "${mod}+6" = switch 6;
-            "${mod}+7" = switch 7;
-            "${mod}+8" = switch 8;
-            "${mod}+9" = switch 9;
-            "${mod}+0" = switch 0;
+            "${mod}+1" = switch "1";
+            "${mod}+2" = switch "2";
+            "${mod}+3" = switch "3";
+            "${mod}+4" = switch "4";
+            "${mod}+5" = switch "5";
+            "${mod}+6" = switch "6";
+            "${mod}+7" = switch "7";
+            "${mod}+8" = switch "8";
+            "${mod}+9" = switch "9";
+            "${mod}+0" = switch "0";
+
+            "${mod}+Ctrl+l" = "exec --no-startup-id ${pkgs.i3lock}/bin/i3lock -n -c 111111";
+            "${mod}+Ctrl+s" = "exec --no-startup-id ${pkgs.flameshot}/bin/flameshot gui";
+            "${mod}+Ctrl+n" = "exec --no-startup-id ${pkgs.gnome3.nautilus}/bin/nautilus";
+
+            # Pulse Audio controls
+            "XF86AudioRaiseVolume" = "exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 +5%";
+            "XF86AudioLowerVolume" = "exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-sink-volume 0 -5%";
+            "XF86AudioMute" = "exec --no-startup-id ${pkgs.pulseaudio}/bin/pactl set-sink-mute 0 toggle";
+
+            # Sreen brightness controls
+            "XF86MonBrightnessUp" = "exec ${pkgs.xorg.xbacklight}/bin/xbacklight -inc 20";
+            "XF86MonBrightnessDown" = "exec ${pkgs.xorg.xbacklight}/bin/xbacklight -dec 20";
+
+            # Media player controls
+            "XF86AudioPlay" = "exec ${pkgs.playerctl}/bin/playerctl play";
+            "XF86AudioPause" = "exec ${pkgs.playerctl}/bin/playerctl pause";
+            "XF86AudioNext" = "exec ${pkgs.playerctl}/bin/playerctl next";
+            "XF86AudioPrev" = "exec ${pkgs.playerctl}/bin/playerctl previous";
           };
       };
     };
 
     programs.i3status-rust = {
       enable = true;
+      bars = {
+        default = {
+          blocks = [
+            {
+              block = "disk_space";
+              path = "/";
+              alias = "/";
+              info_type = "available";
+              unit = "GB";
+              interval = 60;
+              warning = 20.0;
+              alert = 10.0;
+            }
+            # {
+            #   block = "temperature";
+            # }
+            (if config.dotfiles.desktop.laptop then {
+              block = "battery";
+              allow_missing = true;
+            } else {})
+            {
+              block = "memory";
+              display_type = "memory";
+              format_mem = "{mem_used_percents}";
+              format_swap = "{swap_used_percents}";
+            }
+            {
+              block = "cpu";
+              interval = 1;
+              format = "{utilization} {frequency}";
+            }
+            {
+              block = "net";
+              interval = 2;
+              hide_inactive = true;
+            }
+            {
+              block = "load";
+              interval = 1;
+              format = "{1m}";
+            }
+            { block = "sound"; }
+            {
+              block = "time";
+              interval = 60;
+              format = "%a %d/%m %R";
+            }
+          ];
+          settings = {
+            theme =  {
+              name = "nord-dark";
+              overrides = {
+                idle_bg = base01;
+                # idle_fg = "#abcdef";
+              };
+            };
+          };
+          icons = "awesome5";
+          theme = "nord-dark";
+        };
+      };
     };
 
     home.packages = with pkgs; [
@@ -126,6 +254,10 @@ in {
 
     xsessionInitExtra = mkOption {
       type = types.str;
+
+# Sreen brightness controls
+bindsym XF86MonBrightnessUp exec xbacklight -inc 20 # increase screen brightness
+bindsym XF86MonBrightnessDown exec xbacklight -dec 20 # decrease screen brightness
       default = "";
     };
   };
