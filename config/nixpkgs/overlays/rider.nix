c43b1dac24f00e82551e56c60bfa6c74f490a8d3
Jonas Juselius <jonas.juselius@itpartner.no>
2022-02-16 15:07:48 +0100

    add rider eap-2022.1

--- config/nixpkgs/overlays/rider.nix
+++ config/nixpkgs/overlays/rider.nix
@@ -1,4 +1,7 @@
 self: super:
+let
+  eap = "EAP2-221.4165.164.Checked";
+in
 {
   rider = super.jetbrains.rider.overrideAttrs (attrs: rec {
       version = "2021.3.3";
@@ -7,6 +10,16 @@ self: super:
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
         sha256 = "13q6hk5l3fqmz818z5wj014jd5iglpdcpi8zlpgaim1jg5fpvi8x";
+      };
+  });
+
+  rider-eap = super.jetbrains.rider.overrideAttrs (attrs: rec {
+      version = "2022.1";
+      name = "rider-${version}";
+
+      src = super.fetchurl {
+        url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}-${eap}.tar.gz";
+        sha256 = "1chwsin7i735m9955652j48ra9bfpm4d2a6hpcxcdiqs3szifjrd";
       };
   });
 }
