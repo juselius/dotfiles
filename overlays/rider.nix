17398e5145aaca6afdc1d6b8c6ebaf260b1c9437
Jonas Juselius <jonas.juselius@tromso.serit.no>
2022-04-14 08:31:13 +0200

    fix: update rider and make eap default

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -1,15 +1,14 @@
 self: super:
 let
-  eap = "EAP9-221.5080.110.Checked";
-in
-{
-  rider = super.jetbrains.rider.overrideAttrs (attrs: rec {
-      version = "2021.3.3";
+  eap = "EAP10-221.5080.209";
+
+  rider-stable = super.jetbrains.rider.overrideAttrs (attrs: rec {
+      version = "2021.3.4";
       name = "rider-${version}";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-        sha256 = "13q6hk5l3fqmz818z5wj014jd5iglpdcpi8zlpgaim1jg5fpvi8x";
+        sha256 = "0g20wan79qwrm8ml1ns0ma3fcqajjmixzk5sgj0n4dhkzszkq4qj";
       };
   });
 
@@ -19,7 +18,10 @@ in
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}-${eap}.tar.gz";
-        sha256 = "0hrvcl0jfmhhijvnbyd8m4slk15gbh70a2fyiinx724bm85y196b";
+        sha256 = "0lkzb3a1ky8cwsnb64bqway9qm62xgaw84vjilbjc2s1znxkfvj7";
       };
   });
+in
+{
+  rider = rider-eap;
 }
