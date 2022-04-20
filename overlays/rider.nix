2031281517b9f1267a643a4486e06e447ab0dbe2
Jonas Juselius <jonas.juselius@itpartner.no>
2022-04-20 12:23:55 +0200

    fix: update rider to 2022.1

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -3,12 +3,12 @@ let
   eap = "EAP10-221.5080.209";
 
   rider-stable = super.jetbrains.rider.overrideAttrs (attrs: rec {
-      version = "2021.3.4";
+      version = "2022.1";
       name = "rider-${version}";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-        sha256 = "0g20wan79qwrm8ml1ns0ma3fcqajjmixzk5sgj0n4dhkzszkq4qj";
+        sha256 = "0ws66qh3i1fwsd3zzhnswipr8m6nhvrf8blx54bzzx2jyf5h3lp5";
       };
   });
 
@@ -23,5 +23,5 @@ let
   });
 in
 {
-  rider = rider-eap;
+  rider = rider-stable;
 }
