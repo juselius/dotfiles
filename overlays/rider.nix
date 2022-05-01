651216a2a78baeda504ee29ef71f303f768d16f0
Jonas Juselius <jonas.juselius@tromso.serit.no>
2022-05-01 22:11:35 +0200

    fix: udpdate rider to 2022.1.1

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -1,17 +1,17 @@
 self: super:
 let
-  eap = "EAP10-221.5080.209";
 
   rider-stable = super.jetbrains.rider.overrideAttrs (attrs: rec {
-      version = "2022.1";
+      version = "2022.1.1";
       name = "rider-${version}";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-        sha256 = "0ws66qh3i1fwsd3zzhnswipr8m6nhvrf8blx54bzzx2jyf5h3lp5";
+        sha256 = "0p5xn2nj0wfs5d9hcfxx2xs6qgf46k42n68lq53li7kw2nrbl6fs";
       };
   });
 
+  eap = "EAP10-221.5080.209";
   rider-eap = super.jetbrains.rider.overrideAttrs (attrs: rec {
       version = "2022.1";
       name = "rider-${version}";
