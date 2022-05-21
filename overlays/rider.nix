4d67ff022207f16197f2c9671f6fbae0b5f3204d
Jonas Juselius <jonas.juselius@tromso.serit.no>
2022-05-21 08:59:38 +0200

    fix: upadte rider to 2022 eap

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -11,17 +11,17 @@ let
       };
   });
 
-  eap = "EAP10-221.5080.209";
+  eap = "EAP1-222.2270.26.Checked";
   rider-eap = super.jetbrains.rider.overrideAttrs (attrs: rec {
-      version = "2022.1";
+      version = "2022.2";
       name = "rider-${version}";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}-${eap}.tar.gz";
-        sha256 = "0lkzb3a1ky8cwsnb64bqway9qm62xgaw84vjilbjc2s1znxkfvj7";
+        sha256 = "14pn2mwzn32j8jh00wlcws6gibd0x3q2wzd8j8jjg9v5g9zraqza";
       };
   });
 in
 {
-  rider = rider-stable;
+  rider = rider-eap;
 }
