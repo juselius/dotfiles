a07d7528d6e1c3ce2d1004111344fe6416f6a247
Jonas Juselius <jonas.juselius@tromso.serit.no>
2022-08-17 08:21:26 +0200

    wip: rider 2022.2.1

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -20,8 +20,74 @@ let
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}-${eap}.tar.gz";
         sha256 = "sha256-ZvamztxmRoFc1aro+QbwCcOOJzMkk2ijamiSTcSkZbc=";
       };
+
+      postPatch =
+        attrs.postPatch + ''
+        patchelf --set-interpreter $interpreter lib/ReSharperHost/linux-x64/Rider.Backend
+        '';
+  });
+
+  rpath = with super; super.lib.makeLibraryPath [ stdenv.cc.cc.lib zlib ];
+  rider-latest = super.jetbrains.rider.overrideAttrs (attrs: rec {
+      version = "2022.2.1";
+      name = "rider-${version}";
+
+      src = super.fetchurl {
+        url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
+        sha256 = "sha256-P2HF+99GuUpct3KtUd2lqcrYqceteNvKzwHaoiJqVjM=";
+      };
+
+      postPatch =
+        attrs.postPatch + ''
+        patchelf --set-interpreter $interpreter lib/ReSharperHost/linux-x64/Rider.Backend
+        patchelf --set-rpath ${rpath} lib/ReSharperHost/linux-x64/Rider.Backend
+        for i in \
+            jbr/bin/java \
+            jbr/bin/javac \
+            jbr/bin/javadoc \
+            jbr/bin/jcmd \
+            jbr/bin/jdb \
+            jbr/bin/jfr \
+            jbr/bin/jhsdb \
+            jbr/bin/jinfo \
+            jbr/bin/jmap \
+            jbr/bin/jps \
+            jbr/bin/jrunscript \
+            jbr/bin/jstack \
+            jbr/bin/jstat \
+            jbr/bin/keytool \
+            jbr/bin/rmiregistry \
+            jbr/bin/serialver \
+            jbr/lib/chrome-sandbox \
+            jbr/lib/jcef_helper \
+            jbr/lib/jexec \
+            jbr/lib/jspawnhelper; do
+          patchelf --set-interpreter $interpreter $i
+          patchelf --add-rpath ${rpath} $i
+        done
+        for i in \
+            plugins/cidr-debugger-plugin/bin/lldb/linux/bin/LLDBFrontend \
+            plugins/cidr-debugger-plugin/bin/lldb/linux/bin/lldb \
+            plugins/cidr-debugger-plugin/bin/lldb/linux/bin/lldb-argdumper \
+            plugins/cidr-debugger-plugin/bin/lldb/linux/bin/lldb-server \
+            plugins/dotCommon/DotFiles/linux-x64/JetBrains.Profiler.PdbServer \
+            plugins/remote-dev-server/selfcontained/bin/Xvfb \
+            plugins/remote-dev-server/selfcontained/bin/xkbcomp; \
+        do patchelf --set-interpreter $interpreter $i; done
+        sed -i '
+            s/runtime\.sh/runtime-dotnet.sh/;
+            /^set/a export DOTNET_ROOT=${super.dotnet-sdk_6}' lib/ReSharperHost/Rider.Backend.sh
+        '';
+
+      postInstall = ''
+        cd $out/rider/lib/ReSharperHost/linux-x64
+        rm -rf dotnet
+        ln -s ${super.dotnet-sdk_6} dotnet
+        cd ..
+        ln -s ${super.dotnet-sdk_6}/shared/Microsoft.NETCore.App/6.0.5/libhostpolicy.so .
+      '';
   });
 in
 {
-  rider = rider-eap;
+  rider = rider-latest;
 }
