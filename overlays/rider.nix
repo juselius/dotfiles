aeb19d1af7192472857f852bc18c9f3693cdb7eb
Jonas Juselius <jonas.juselius@tromso.serit.no>
2022-08-24 15:24:05 +0200

    fix: update rider to 2022.2.2

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -29,12 +29,12 @@ let
 
   rpath = with super; super.lib.makeLibraryPath [ stdenv.cc.cc.lib zlib ];
   rider-latest = super.jetbrains.rider.overrideAttrs (attrs: rec {
-      version = "2022.2.1";
+      version = "2022.2.2";
       name = "rider-${version}";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-        sha256 = "sha256-P2HF+99GuUpct3KtUd2lqcrYqceteNvKzwHaoiJqVjM=";
+        sha256 = "sha256-WX+JdpvdeYZE3eM4YW9kJFcuRwRAFNpresg7laPWez8=";
       };
 
       postPatch =
