72167b1400f70cc655cfbc704e7e2ba632fa8dde
Jonas Juselius <jonas.juselius@itpartner.no>
2022-09-02 12:13:54 +0200

    fix: update install script

--- scripts/install-home-manager.sh
+++ scripts/install-home-manager.sh
@@ -7,12 +7,13 @@ cd $top
 git submodule init
 git submodule update
 
-nix-channel --add https://github.com/nix-community/home-manager/archive/release-21.11.tar.gz home-manager
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-22.05.tar.gz home-manager
 nix-channel --update
 
 export NIX_PATH="home-manager=$HOME/.nix-defexpr/channels/home-manager${NIX_PATH:+:}$NIX_PATH"
 nix-shell '<home-manager>' -A install
 
 mkdir -p ~/.config/omf
+rm ~/.config/nixpkgs/home.nix
 
 ~/.nix-profile/bin/home-manager switch -f $top
