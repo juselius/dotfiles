f5f88b9b947ae2cd759176a4773c17451df9f2fe
Jonas Juselius <jonas.juselius@itpartner.no>
2022-10-07 10:28:46 +0200

    fix: update dotnet 6 and add dotnet 7 preview

--- overlays/dotnet-sdk.nix
+++ overlays/dotnet-sdk.nix
@@ -6,10 +6,18 @@ let
 in
 {
   dotnet-sdk_6 = super.dotnetCorePackages.sdk_6_0.overrideAttrs (attrs: rec {
-    version = "6.0.400";
+    version = "6.0.401";
     src = super.fetchurl {
       url = "https://dotnetcli.azureedge.net/dotnet/Sdk/${version}/${pname}-${version}-${platform}-${suffix}.tar.gz";
-      sha512 = "sha512-jey7oKawlQHa7eUsu1qa6eXzGt4gGRjAPvzRtMw0Xsk0+IMhcE7DvrH5DyIEk0vnJZx29m2SBMvdFZM1gmAnYw==";
+      sha512 = "sha512-b85fKebPyA2h34bS3jpjcQgCM5fSdeDc+gt57zbrhcLDQz20Z6pdj9p+MrwhIFoSZja1PVbE60xUfZ07I3DLMQ==";
+    };
+  });
+
+  dotnet-sdk_7 = super.dotnetCorePackages.sdk_6_0.overrideAttrs (attrs: rec {
+    version = "7.0.100-rc.1.22431.12";
+    src = super.fetchurl {
+      url = "https://dotnetcli.azureedge.net/dotnet/Sdk/${version}/${pname}-${version}-${platform}-${suffix}.tar.gz";
+      sha256 = "sha256-WAnEwKNFuvTDDo1vS1qT33cr2tOtmCq+ysLS1uHtk+E=";
     };
   });
 
