42f5c2c3bb28cb9cffcc60f975d2717e5d8bb729
Jonas Juselius <jonas.juselius@itpartner.no>
2022-10-27 14:29:02 +0200

    fix: du/duf shows only local and network devices

--- config/fish/functions/kcc.fish
+++
@@ -1,3 +0,0 @@
-function kcc --wraps='kubectl config use-context' --description 'alias kcc=kubectl config use-context'
-   kubectl config use-context $argv;
-end
--- config/fish/functions/kcd.fish
+++
@@ -1,7 +0,0 @@
-function kcd --wraps='kubectl config set-context --current -n' --description 'alias kcd=kubectl config set-context --current --namespace='
-  if test (count $argv) = 0;
-      kubectl config set-context --current --namespace=default;
-  else
-      kubectl config set-context --current --namespace=$argv;
-  end
-end
