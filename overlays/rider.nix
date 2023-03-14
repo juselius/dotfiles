f66b670a0fc06991b07ed78885a687364d20dd48
Jonas Juselius <jonas.juselius@itpartner.no>
2023-03-14 10:49:07 +0100

    fix: update rider-eap 8

--- overlays/rider.nix
+++ overlays/rider.nix
@@ -45,14 +45,14 @@ let
   jetbrainsNix = "/nix/var/nix/profiles/per-user/root/channels/nixos/pkgs/applications/editors/jetbrains";
   jetbrains = super.callPackage jetbrainsNix { jdk = super.jdk; };
 
-  eap = "EAP7-231.7864.80";
+  eap = "EAP8-231.8109.48";
   rider-eap = jetbrains.rider.overrideAttrs (attrs: rec {
       version = "2023.1";
       name = "rider-${version}";
 
       src = super.fetchurl {
         url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}-${eap}.Checked.tar.gz";
-        sha256 = "sha256-KHVEij42GZh88PDOAOI4Bjh8C2uulNBU7EwakOimOvc=";
+        sha256 = "sha256-EJlCrcsayc/bckMSVk0qJk6pA0yIyd3Wr5Ie3pBS9pk=";
       };
 
       postPatch = patch attrs;
