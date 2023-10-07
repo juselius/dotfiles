295d7173a44bc27be5414ba2c2afb55d66255496
Jonas Juselius <jonas.juselius@tromso.serit.no>
2023-10-07 06:32:38 +0200

    fix: update tilt overlay, don't understand why overrides don't work, but...

--- overlays/tilt.nix
+++ overlays/tilt.nix
@@ -1,13 +1,34 @@
 self: super:
-{
-  tilt = super.tilt.overrideAttrs (attrs: rec {
-      version = "0.33.4";
+let
+    tilt =
+        super.buildGoModule rec {
+          pname = "tilt";
+          /* Do not use "dev" as a version. If you do, Tilt will consider itself
+            running in development environment and try to serve assets from the
+            source tree, which is not there once build completes.  */
+          version = "0.33.6";
 
-      src = super.fetchFromGitHub {
-          owner  = "tilt-dev";
-          repo   = attrs.pname;
-          rev    = "v${version}";
-          sha256 = "sha256-rQ5g5QyGyuJAHmE8zGFzqtpqW2xEju5JV386y9Cn+cs=";
-      };
-  });
+          src = super.fetchFromGitHub {
+            owner = "tilt-dev";
+            repo = "tilt";
+            rev = "v${version}";
+            hash = "sha256-WtE8ExUKFRtdYeg0+My/DB+L/qT+J1EaKHKChNjC5oI=";
+          };
+
+          vendorHash = null;
+
+          subPackages = [ "cmd/tilt" ];
+
+          ldflags = [ "-X main.version=${version}" ];
+
+          meta = {
+            description = "Local development tool to manage your developer instance when your team deploys to Kubernetes in production";
+            homepage = "https://tilt.dev/";
+            license = super.lib.licenses.asl20;
+            maintainers = with super.lib.maintainers; [ anton-dessiatov ];
+          };
+        };
+in
+{
+    inherit tilt;
 }
