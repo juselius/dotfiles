640fcb8f02bfd1ab27c1ba533e91041975189ce6
Jonas Juselius <jonas.juselius@itpartner.no>
2023-11-07 10:06:13 +0100

    fix: fix tilt infinite recursion trouble

--- overlays/tilt.nix
+++ overlays/tilt.nix
@@ -1,5 +1,22 @@
 # nix-prefetch-git https://github.com/tilt-dev/tilt --rev v0.33.5
 self: super:
+let
+  args = rec {
+      /* Do not use "dev" as a version. If you do, Tilt will consider itself
+        running in development environment and try to serve assets from the
+        source tree, which is not there once build completes.  */
+      version = "0.33.6";
+
+      src = super.fetchFromGitHub {
+        owner = "tilt-dev";
+        repo = "tilt";
+        rev = "v${version}";
+        hash = "sha256-WtE8ExUKFRtdYeg0+My/DB+L/qT+J1EaKHKChNjC5oI=";
+      };
+  };
+
+ tilt-assets = super.callPackage ./tilt/assets.nix args;
+in
 {
   untilt = super.tilt.overrideAttrs (attrs: rec {
       version = "0.33.5";
@@ -12,6 +29,6 @@ self: super:
       };
   });
 
-  tilt = super.callPackage ./tilt/default.nix {};
+  tilt = super.callPackage ./tilt/binary.nix (args // { inherit tilt-assets; });
 }
 
