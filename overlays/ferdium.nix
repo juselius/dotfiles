7bd84ef567f1c428dca13c58866e2ae43b00f80e
Jonas Juselius <jonas.juselius@oceanbox.io>
2024-05-19 09:24:31 +0200

    fix: update ferdium

--- overlays/ferdium.nix
+++ overlays/ferdium.nix
@@ -1,14 +1,14 @@
 self: super:
 let
     arch = "amd64";
-    version = "6.7.3";
+    version = "6.7.4";
 in
 {
   ferdium = super.ferdium.overrideAttrs (attrs: rec {
       inherit version;
       src = super.fetchurl {
         url = "https://github.com/ferdium/ferdium-app/releases/download/v${version}/Ferdium-linux-${version}-${arch}.deb";
-        hash = "sha256-Q5fUMQQTQKgUkakmsiBFH2UwJdk/XV8wpQi6uRHi90U=";
+        hash = "sha256-4yMaW1ByEQaQHdn99TGpUez2jVi5Ad4ODpugzW6kTm8=";
       };
   });
 }
