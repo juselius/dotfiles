b134264c3283ca0aad6675f6f62b27fde1ded675
Jonas Juselius <jonas.juselius@oceanbox.io>
2024-08-08 08:34:06 +0200

    fix: add guard to mirrord fixup script

--- local/bin/fixup_mirrord.sh
+++ local/bin/fixup_mirrord.sh
@@ -3,9 +3,11 @@
 
 set interp (nix-instantiate --eval -E 'with import <nixpkgs> {}; "${pkgs.glibc.outPath}/lib64/ld-linux-x86-64.so.2"' | tr -d \")
 
-for i in ~/.local/bin/mirrord ~/.local/share/JetBrains/Rider20*/mirrord/bin/linux/x86-64/mirrord;
-    echo $i $interp
-    chmod 755 $i
-    patchelf --set-interpreter $interp $i
+for i in ~/.local/bin/mirrord ~/.local/share/JetBrains/Rider20*/mirrord/bin/linux/x86-64/mirrord
+    if [ -f $i ]
+        echo $i $interp
+        chmod 755 $i
+        patchelf --set-interpreter $interp $i
+    end
 end
 
