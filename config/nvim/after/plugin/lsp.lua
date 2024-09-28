52e1566b563b6264823ed2d5b6902e13c026534e
Jonas Juselius <jonas.juselius@itpartner.no>
2024-09-28 09:00:46 +0200

    fix: fix vim lsp bindings

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,24 +1,24 @@
 -- My lsp configs...
 vim.g.lsp_zero_extend_lspconfig = 0
 
+local lspconfig = require("lspconfig")
 local lsp_zero = require("lsp-zero")
--- lsp_zero.extend_lspconfig()
+local cmp = require("cmp")
+local cmp_action = lsp_zero.cmp_action()
 
-local cmp = require'cmp'
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
-local cmp_mappings = lsp_zero.defaults.cmp_mappings({
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-    ['<C-Space>'] = cmp.mapping.complete(),
-})
 
--- cmp_mappings['<Tab>'] = nil
--- cmp_mappings['<S-Tab>'] = nil
--- cmp_mappings['<CR>'] = nil
+-- NOTE: Ignore the extend_lspconfig error on startup
+vim.g.lsp_zero_extend_lspconfig = 0
+
+-- Se `:h cmp` for flere valg
+cmp.setup({
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<Up>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<Down>'] = cmp.mapping.select_next_item(cmp_select),
+  }),
+})
 
 lsp_zero.on_attach(function(client, bufnr)
     lsp_zero.default_keymaps({ buffer = bufnr })
@@ -37,6 +37,6 @@ lsp_zero.setup_servers({
 })
 
 local lua_opts = lsp_zero.nvim_lua_ls()
-require('lspconfig').lua_ls.setup(lua_opts)
+lspconfig.lua_ls.setup(lua_opts)
 
 lsp_zero.setup()
