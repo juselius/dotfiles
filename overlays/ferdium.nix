9213d4d90844f20e124220d808b6ea6d7c3c3b95
Jonas Juselius <jonas.juselius@itpartner.no>
2024-10-15 10:13:55 +0200

    fix: update ferdium, now with sandboxes!

--- overlays/ferdium.nix
+++ overlays/ferdium.nix
@@ -1,14 +1,14 @@
 self: super:
 let
     arch = "amd64";
-    version = "6.7.7";
+    version = "6.7.8-nightly.6";
 in
 {
   ferdium = super.ferdium.overrideAttrs (attrs: rec {
       inherit version;
       src = super.fetchurl {
         url = "https://github.com/ferdium/ferdium-app/releases/download/v${version}/Ferdium-linux-${version}-${arch}.deb";
-        hash = "sha256-5OW10sABNNYQNUgorM634j5oiBhJJby1ymH6UcmknRg=";
+        hash = "sha256-yNQy/4j11MNObW4B58mjkAXCBwdYcIQxsJ4tdDnL6+w=";
       };
   });
 }
