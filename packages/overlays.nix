3ff35ea7720281b7f9efdf75bb68e0147e7a331a
Jonas Juselius <jonas.juselius@oceanbox.io>
2025-05-12 12:00:12 +0200

    fix: disable dotnet and linkerd overlays

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -1,13 +1,13 @@
 # do not move this file to ../overlays, it breaks home-manager badly
 let
   overlays = [
-    ../overlays/dotnet-sdk.nix
-    ../overlays/linkerd.nix
     ../overlays/rider.nix
     ../overlays/ferdium.nix
     ../overlays/vcluster.nix
     ../overlays/fcitx.nix
     ../overlays/kubelogin.nix
+    # ../overlays/dotnet-sdk.nix
+    # ../overlays/linkerd.nix
     # ../overlays/vscode.nix
     # ../overlays/wavebox.nix
     # ../overlays/tilt.nix
