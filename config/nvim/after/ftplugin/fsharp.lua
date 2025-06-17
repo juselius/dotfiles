ae20c5fd85c25a41cdae5e1003da9ff38423a089
Jonas Juselius <jonas.juselius@oceanbox.io>
2025-06-17 13:22:55 +0200

    fix: misc nvim annoyance fixes

--- config/nvim/after/ftplugin/fsharp.lua
+++ config/nvim/after/ftplugin/fsharp.lua
@@ -3,3 +3,4 @@ vim.opt_local.number = true;
 vim.opt_local.relativenumber = true;
 vim.opt_local.textwidth = 120;
 vim.opt_local.colorcolumn = "+1";
+vim.opt_local.commentstring = "// %s";
--- config/nvim/after/plugin/telescope.lua
+++ config/nvim/after/plugin/telescope.lua
@@ -1,9 +1,22 @@
 local telescope = require('telescope')
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
 
-telescope.setup()
+telescope.setup(
+    {
+        defaults = {
+            mappings = {
+                i = {
+                    ["<esc>"] = actions.close,
+                },
+            },
+        },
+    }
+)
 
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>p", builtin.find_files, {})
-vim.keymap.set("n", "<leader>rg", builtin.live_grep, {})
-vim.keymap.set("n", "<leader>ff", builtin.live_grep, {})
+vim.keymap.set("n", "<C-l>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+
