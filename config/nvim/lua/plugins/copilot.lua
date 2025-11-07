9eb2f1fd314fc582ddf067e71fed501b596dba4a
Jonas Juselius <jonas.juselius@oceanbox.io>
2025-11-07 08:52:37 +0100

    fix: tweak copilot settings

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -4,17 +4,17 @@ return {
   build = ":Copilot auth",
   event = "BufReadPost",
   opts = {
-    suggestion = {
-      enabled = not vim.g.ai_cmp,
-      auto_trigger = true,
-      hide_during_completion = vim.g.ai_cmp,
-      keymap = {
-        accept = false, -- handled by nvim-cmp / blink.cmp
-        next = "<M-]>",
-        prev = "<M-[>",
-      },
-    },
-    panel = { enabled = false },
+    -- suggestion = {
+    --   enabled = not vim.g.ai_cmp,
+    --   auto_trigger = true,
+    --   hide_during_completion = vim.g.ai_cmp,
+    --   keymap = {
+    --     accept = false, -- handled by nvim-cmp / blink.cmp
+    --     next = "<M-]>",
+    --     prev = "<M-[>",
+    --   },
+    -- },
+    -- panel = { enabled = false },
     filetypes = {
       fsharp = true,
       lua = true,
