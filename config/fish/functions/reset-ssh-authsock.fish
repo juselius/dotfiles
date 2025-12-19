014ca29b11e071611d168efb0f78d76437f7a89c
Jonas Juselius <jonas.juselius@oceanbox.io>
2025-12-19 10:45:25 +0100

    fix: use simkir's robust ssh agent reattach function

--- config/fish/functions/reset-ssh-authsock.fish
+++ config/fish/functions/reset-ssh-authsock.fish
@@ -1,5 +1,22 @@
+function __find_newest_sock
+    fd --owner $USER "agent.*" /tmp/ssh-* \
+        | xargs ls -lt \
+        | head -1 \
+        | rev \
+        | cut -d' ' -f1 \
+        | rev
+end
+
 function reset-ssh-authsock
-    if test (fd --owner $USER --max-depth 1 . /tmp |grep ssh- |count) -gt 0
-        set -x SSH_AUTH_SOCK (fd --owner $USER agent. /tmp/ssh-* | head -1)
+    if test -n "$SSH_AUTH_SOCK"
+        if not test -e "$SSH_AUTH_SOCK"
+            set -l sock (__find_newest_sock)
+            echo "Found $sock"
+            set -xg SSH_AUTH_SOCK $sock
+        else
+            echo "SSH_AUTH_SOCK is not a file: $SSH_AUTH_SOCK"
+        end
+    else
+        echo "SSH_AUTH_SOCK is not set"
     end
 end
