b46f76820b2c1931143f6660562f09b2e17628bc
Jonas Juselius <jonas.juselius@oceanbox.io>
2026-03-12 13:26:08 +0100

    feat: disable mega-irritating mini-pair plugin and enable markdown-render

--- config/nvim/lua/plugins/disabled.lua
+++ config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,9 @@
 return {
-  { "folke/noice.nvim",               enabled = false },
-  { "mason-org/mason.nvim",           enabled = false },
-  { "mason-org/mason-lspconfig.nvim", enabled = false },
+  { "folke/noice.nvim",                          enabled = false },
+  { "mason-org/mason.nvim",                      enabled = false },
+  { "mason-org/mason-lspconfig.nvim",            enabled = false },
+  { "mason-org/mason-lspconfig.nvim",            enabled = false },
+  { "zbirenbaum/copilot.lua",                    enabled = false },
+  { "MeanderingProgrammer/render-markdown.nvim", enabled = true },
+  { "nvim-mini/mini.pairs",                      enabled = false },
 }
