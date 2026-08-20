80218efdb89bde50656386af0c8a55d177044147
Jonas Juselius <jonas.juselius@oceanbox.io>
2026-08-20 08:23:54 +0200

    overlays: update rider and ferdium

--- overlays/ferdium.nix
+++ overlays/ferdium.nix
@@ -1,14 +1,14 @@
 self: super:
 let
   arch = "amd64";
-  version = "7.1.2";
+  version = "7.2.0";
 in
 {
   ferdium = super.ferdium.overrideAttrs (attrs: rec {
     inherit version;
     src = super.fetchurl {
       url = "https://github.com/ferdium/ferdium-app/releases/download/v${version}/Ferdium-linux-${version}-${arch}.deb";
-      hash = "sha256-ODQKFjBa2riJY26aPaAfLzuCyLYkB5oYSxIE28nMmwY=";
+      hash = "sha256-Gk9Swdk6jDoN8UzmdPnoslzrvN+7H5y//HPeB5GqEY8=";
     };
   });
 }
--- overlays/rider.nix
+++ overlays/rider.nix
@@ -1,12 +1,16 @@
 self: super:
 let
-  rpath = with super; super.lib.makeLibraryPath [ stdenv.cc.cc.lib zlib ];
+  rpath =
+    with super;
+    super.lib.makeLibraryPath [
+      stdenv.cc.cc.lib
+      zlib
+    ];
 
   loName = super.lib.toLower "Rider";
   hiName = super.lib.toUpper "Rider";
-  vmoptsName = loName
-    + super.lib.optionalString super.stdenv.hostPlatform.is64bit "64"
-    + ".vmoptions";
+  vmoptsName =
+    loName + super.lib.optionalString super.stdenv.hostPlatform.is64bit "64" + ".vmoptions";
 
   postPatch = with super; ''
     get_file_size() {
@@ -48,84 +52,89 @@ let
         exit 1
       fi
     fi
-    echo -Djna.library.path=${lib.makeLibraryPath ([
-      libsecret e2fsprogs libnotify
-      # Required for Help -> Collect Logs
-      # in at least rider and goland
-      udev
-    ])} >> $vmopts_file
+    echo -Djna.library.path=${
+      lib.makeLibraryPath ([
+        libsecret
+        e2fsprogs
+        libnotify
+        # Required for Help -> Collect Logs
+        # in at least rider and goland
+        udev
+      ])
+    } >> $vmopts_file
   '';
 
-  patch = attrs:
-        postPatch + ''
-        patchelf --set-interpreter $interpreter lib/ReSharperHost/linux-x64/Rider.Backend
-        patchelf --set-rpath ${rpath} lib/ReSharperHost/linux-x64/Rider.Backend
-        for i in \
-            plugins/cidr-debugger-plugin/bin/lldb/linux/x64/bin/LLDBFrontend \
-            plugins/cidr-debugger-plugin/bin/lldb/linux/x64/bin/lldb \
-            plugins/cidr-debugger-plugin/bin/lldb/linux/x64/bin/lldb-argdumper \
-            plugins/cidr-debugger-plugin/bin/lldb/linux/x64/bin/lldb-server \
-            plugins/dotCommon/DotFiles/linux-x64/JetBrains.Profiler.PdbServer \
-            plugins/remote-dev-server/selfcontained/bin/Xvfb \
-            plugins/remote-dev-server/selfcontained/bin/xkbcomp; \
-        do patchelf --set-interpreter $interpreter $i; done
-        sed -i 's/runtime\.sh/runtime-dotnet.sh/' lib/ReSharperHost/Rider.Backend.sh
+  patch =
+    attrs:
+    postPatch
+    + ''
+      patchelf --set-interpreter $interpreter lib/ReSharperHost/linux-x64/Rider.Backend
+      patchelf --set-rpath ${rpath} lib/ReSharperHost/linux-x64/Rider.Backend
+      for i in \
+          plugins/cidr-debugger-plugin/bin/lldb/linux/x64/bin/LLDBFrontend \
+          plugins/cidr-debugger-plugin/bin/lldb/linux/x64/bin/lldb \
+          plugins/cidr-debugger-plugin/bin/lldb/linux/x64/bin/lldb-argdumper \
+          plugins/cidr-debugger-plugin/bin/lldb/linux/x64/bin/lldb-server \
+          plugins/dotCommon/DotFiles/linux-x64/JetBrains.Profiler.PdbServer \
+          plugins/remote-dev-server/selfcontained/bin/Xvfb \
+          plugins/remote-dev-server/selfcontained/bin/xkbcomp; \
+      do patchelf --set-interpreter $interpreter $i; done
+      sed -i 's/runtime\.sh/runtime-dotnet.sh/' lib/ReSharperHost/Rider.Backend.sh
 
-        # NOTE(simkir): Replacing their net7 dotnet with our net8. Wasn't allowed
-        # to do this in the postInstall step, so doing it here.
-        # rm -rf lib/ReSharperHost/linux-x64/dotnet
-        # ln -sf ${super.dotnet_9.sdk} lib/ReSharperHost/linux-x64/dotnet
-        '';
+      # NOTE(simkir): Replacing their net7 dotnet with our net8. Wasn't allowed
+      # to do this in the postInstall step, so doing it here.
+      # rm -rf lib/ReSharperHost/linux-x64/dotnet
+      # ln -sf ${super.dotnet_9.sdk} lib/ReSharperHost/linux-x64/dotnet
+    '';
 
   rider-latest = super.jetbrains.rider.overrideAttrs (attrs: rec {
-    version = "2025.1.7";
+    version = "2026.2.0.2";
     name = "rider-${version}";
 
     src = super.fetchurl {
       url = "https://download.jetbrains.com/rider/JetBrains.Rider-${version}.tar.gz";
-      sha256 = "sha256-YmW5aInJBkxP03rv6hxQBG7/yOt1YJ5wek36qRzXEoA=";
+      sha256 = "sha256-61cMsfnNeFGEvpZhOIlBfbcC7lYIKbJluRuGMaqDkLE=";
     };
 
-    postInstall =
-      with super;
-      let
-        WLlibs = lib.makeLibraryPath ([
-          stdenv.cc.cc.lib
-          zlib
-          wayland
-          freetype
-          fontconfig
-        ]);
-      in
-    ''
-      cd $out/rider
+    # postInstall =
+    #   with super;
+    #   let
+    #     WLlibs = lib.makeLibraryPath ([
+    #       stdenv.cc.cc.lib
+    #       zlib
+    #       wayland
+    #       freetype
+    #       fontconfig
+    #     ]);
+    #   in
+    #   ''
+    #     cd $out/rider
+    #
+    #     sed -i 's|-Djna\.library.path=.*|&:${WLlibs}|' bin/${vmoptsName}
+    #     echo "-Dawt.toolkit.name=WLToolkit" >> bin/${vmoptsName}
+    #     echo "-Dsun.java2d.vulkan=true" >> bin/${vmoptsName}
+    #
+    #     ls -d $PWD/plugins/cidr-debugger-plugin/bin/lldb/linux/*/lib/python3.8/lib-dynload/* |
+    #     xargs patchelf \
+    #       --replace-needed libssl.so.10 libssl.so \
+    #       --replace-needed libcrypto.so.10 libcrypto.so \
+    #       --replace-needed libcrypt.so.1 libcrypt.so
+    #
+    #     for dir in lib/ReSharperHost/linux-*; do
+    #       rm -rf $dir/dotnet
+    #       ln -s ${super.dotnetCorePackages.dotnet_9.sdk.unwrapped}/share/dotnet $dir/dotnet
+    #     done
+    #   '';
 
-      sed -i 's|-Djna\.library.path=.*|&:${WLlibs}|' bin/${vmoptsName}
-      echo "-Dawt.toolkit.name=WLToolkit" >> bin/${vmoptsName}
-      echo "-Dsun.java2d.vulkan=true" >> bin/${vmoptsName}
-
-      ls -d $PWD/plugins/cidr-debugger-plugin/bin/lldb/linux/*/lib/python3.8/lib-dynload/* |
-      xargs patchelf \
-        --replace-needed libssl.so.10 libssl.so \
-        --replace-needed libcrypto.so.10 libcrypto.so \
-        --replace-needed libcrypt.so.1 libcrypt.so
-
-      for dir in lib/ReSharperHost/linux-*; do
-        rm -rf $dir/dotnet
-        ln -s ${super.dotnetCorePackages.dotnet_9.sdk.unwrapped}/share/dotnet $dir/dotnet
-      done
-    '';
-
-    buildInputs = attrs.buildInputs ++ [
-      super.kubelogin
-      super.libGL
-      super.xorg.libX11
-    ];
+    # buildInputs = attrs.buildInputs ++ [
+    #   super.kubelogin
+    #   super.libGL
+    #   super.xorg.libX11
+    # ];
   });
-  jetbrains = super.callPackage ../../src/nixpkgs/pkgs/applications/editors/jetbrains/default.nix {};
 in
 {
-  rider = rider-latest;
+  inherit rider-latest;
   # rider = jetbrains.rider;
   # rider-stable = rider-latest;
 }
